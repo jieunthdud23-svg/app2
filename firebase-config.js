20d871723c2a8b98ed7eeb61f0bffc3a222ac159
jieunthdud23-svg <jieunthdud23@gmail.com>
2026-06-12 20:04:32 +0900

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정의 값을 입력하세요.
 // 이 웹 설정값은 공개되어도 되며, 실제 보안은 database.rules.json이 담당합니다.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAMs574IeEsuDKb9tFAWCVt_Ns1cUCpM1w",
+  authDomain: "app2-8235a.firebaseapp.com",
+  databaseURL: "https://app2-8235a-default-rtdb.firebaseio.com",
+  projectId: "app2-8235a",
+  storageBucket: "app2-8235a.firebasestorage.app",
+  messagingSenderId: "929249950075",
+  appId: "1:929249950075:web:2e92020f843467fba4e5de"
 };
